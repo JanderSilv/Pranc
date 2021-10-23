c27f4a5b2007c4282359b8c7557a32243d28c175
Jander Silva <Jander.silv@outlook.com>
2021-10-23 15:12:55 -0300

    chore: add and setup next-react-svg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const withReactSvg = require('next-react-svg')
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  ...withReactSvg({
+    include: path.resolve(__dirname, 'public/assets/icons'),
+    webpack(config) {
+      return config
+    },
+  }),
 }
