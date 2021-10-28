52fee2b2c27dfb89efc173bd656fe6936d727757
Jander Silva <Jander.silv@outlook.com>
2021-10-28 14:42:41 -0300

    feat(Header): implements it

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,50 @@
+import { styled } from '@mui/material'
+import LinkButton from 'src/components/LinkButton'
+
+export const Header = styled('header')(({ theme: { palette, spacing } }) => ({
+  padding: spacing(4),
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  '& > span': {
+    color: palette.primary.contrastText,
+    fontSize: '2.5rem',
+  },
+}))
+
+export const Container = styled('section')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  zIndex: 10,
+})
+
+export const Content = styled('section')(({ theme: { palette } }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  '& > h1': {
+    color: palette.primary.contrastText,
+  },
+}))
+
+export const StartButton = styled(LinkButton)(
+  ({ theme: { palette, spacing } }) => ({
+    width: '100%',
+    maxWidth: '50%',
+
+    marginTop: spacing(2),
+    padding: spacing(1, 4),
+
+    color: palette.primary.contrastText,
+    borderRadius: 30,
+    background: palette.primary.main,
+
+    '&:hover': {
+      background: palette.primary.dark,
+    },
+  })
+)
