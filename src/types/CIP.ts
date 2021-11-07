8321b08519e54856b482bd0488efe40519e28dca
Jander Silva <Jander.silv@outlook.com>
2021-11-07 20:19:23 -0300

    feat(types/CIP): evaluateQuestions return scores by question

--- src/types/CIP.ts
+++ src/types/CIP.ts
@@ -1,5 +1,15 @@
-import IQuestion, { IFuncTable, IQuestionEvaluate } from './IQuestion'
+import IQuestion, { IFuncTable } from './IQuestion'
 import Question from './Question'
+
+export interface Score {
+  cip: string
+  questionsScores: {
+    id: number
+    title: string
+    score: number
+  }[]
+  totalScore: number
+}
 
 export const loadQuestions = async (
   id: number,
@@ -10,10 +20,24 @@ export const loadQuestions = async (
   return rawQuestions.map(rq => new Question(rq, funcs))
 }
 
-export const evaluateQuestions = (questions: Question[]): number => {
-  let result: number = 0
-  questions.forEach(q => {
-    result += q.evaluate()
+export const evaluateQuestions = (
+  cipName: string,
+  questions: Question[]
+): Score => {
+  let totalScore = 0
+  const questionsScores = questions.map(question => {
+    const score = question.evaluate()
+    totalScore += score
+    return {
+      id: question.id,
+      title: question.title,
+      score,
+      solutions: question.solutions,
+    }
   })
-  return result
+  return {
+    cip: cipName,
+    questionsScores,
+    totalScore,
+  }
 }
